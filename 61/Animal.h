782dc1a48df3b7501899224b2d32a01e890b1886
rubdev77 <aleksanyanr681@gmail.com>
2025-11-15 12:40:44 +0400

    61 solution

---
+++ 61/Animal.h
@@ -0,0 +1,19 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+class Animal
+{
+    protected:
+    int legs;
+    int hands;
+    public:
+    Animal(int, int);
+    ~Animal() = default;
+    public:
+    void Walk();
+    void Eat();
+    void Jump();
+    virtual void Voice();
+};
+
+#endif
