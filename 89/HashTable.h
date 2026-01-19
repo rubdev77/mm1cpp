8c97562164f5b02fd9dd57ac67a3785716c20285
rubdev77 <aleksanyanr681@gmail.com>
2026-01-19 12:23:06 +0400

    89 solution

---
+++ 89/HashTable.h
@@ -0,0 +1,66 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+template <typename T>
+class HashTable
+{
+    private:
+    std::vector<std::forward_list<T>> table;
+    static const int MOD = 1009;
+    int hash(const T & x) const;
+    public:
+    HashTable();
+    void insert(const T & x);
+    bool contains(const T & x) const;
+    void erase(const T & x);
+};
+
+template <typename T>
+HashTable<T>::HashTable() : table(MOD)
+{
+
+}
+
+template <typename T>
+int HashTable<T>::hash(const T& x) const
+{
+    return std::hash<T>{}(x) % MOD;
+}
+
+template <typename T>
+void HashTable<T>::insert(const T& x)
+{
+    int h = hash(x);
+
+    for(auto it = table[h].begin(); it != table[h].end(); ++it)
+    {
+        if(*it == x)
+        {
+            return;
+        }
+    }
+    table[h].push_front(x);
+}
+
+template <typename T>
+bool HashTable<T>::contains(const T& x) const
+{
+    int h = hash(x);
+
+    for(auto it = table[h].begin(); it != table[h].end(); ++it)
+    {
+        if(*it == x)
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+void HashTable<T>::erase(const T& x)
+{
+    int h = hash(x);
+
+    table[h].remove(x);
+}
+
+#endif
