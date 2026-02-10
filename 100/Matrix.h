0f7fcd0810fdd0d18dffc69a11cb01d54cc35e29
rubdev77 <aleksanyanr681@gmail.com>
2026-02-10 21:24:36 +0400

    fixed

--- 100/Matrix.h
+++ 100/Matrix.h
@@ -1,7 +1,6 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-
 template <typename T>
 class Matrix
 {
@@ -9,36 +8,52 @@ class Matrix
     std::unique_ptr<T[]> m_data;
     std::size_t r;
     std::size_t c;
-    std::size_t getR() const
-    {
-        return r;
-    }
 
-    std::size_t getC() const
-    {
-        return c;
-    }
     public:
+    Matrix();
+    Matrix(std::size_t rows, std::size_t columns);
 
-    Matrix() : r(0), c(0), m_data(nullptr)
-    {
+    std::size_t getR() const;
+    std::size_t getC() const;
 
-    }
-
-    Matrix(std::size_t rows, std::size_t columns) : r(rows), c(columns), m_data(std::make_unique<T[]>(rows * columns))
-    {
-
-    } 
-
-    T* operator[](int i)
-    {
-        return m_data.get() + (i*c);
-    }
-
-    const T* operator[](int i) const
-    {
-        return m_data.get() + (i*c);
-    }
+    T* operator[](std::size_t i);
+    const T* operator[](std::size_t i) const;
 };
+
+template <typename T>
+Matrix<T>::Matrix() : r(0), c(0), m_data(nullptr)
+{
+
+}
+
+template <typename T>
+Matrix<T>::Matrix(std::size_t rows, std::size_t columns) : r(rows), c(columns), m_data(std::make_unique<T[]>(rows * columns))
+{
+
+}
+
+template <typename T>
+std::size_t Matrix<T>::getR() const
+{
+    return r;
+}
+
+template <typename T>
+std::size_t Matrix<T>::getC() const
+{
+    return c;
+}
+
+template <typename T>
+T* Matrix<T>::operator[](std::size_t i)
+{
+    return m_data.get() + (i * c);
+}
+
+template <typename T>
+const T* Matrix<T>::operator[](std::size_t i) const
+{
+    return m_data.get() + (i * c);
+}
 
 #endif
